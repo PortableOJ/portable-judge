398a46852166e18c29a433d5f2463871b4e3d867
shiroha <keqing.hu@icloud.com>
2022-03-08 00:57:05 +0800

    feat: 增加了日志输出

--- src/runner/runner.h
+++ src/runner/runner.h
@@ -178,6 +178,7 @@ JudgeResultEnum Runner::trace(int pid, int *error, Report *report) {
             case SIGSYS:
                 return JudgeResultEnum::IllegalSystemCall;
             default:
+                Logger::info("exitCode: %", exitCode);
                 return JudgeResultEnum::RuntimeError;
         }
     }
