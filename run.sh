7e65d4c66fbc7d31f84e784ab71ca6aac7ea4d42
shiroha <keqing.hu@icloud.com>
2022-05-01 22:41:41 +0800

    fix: 修复了不会进行预编译的错误

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
-pushd /usr/include/x86_64-linux-gnu/c++/9/bits
-sudo g++ -std=c++17 stdc++.h
+pushd /usr/include/x86_64-linux-gnu/c++
+for file in ./*
+do
+    if test -d ${file}
+    then
+        pushd ${file}/bits
+        sudo g++ -std=c++17 stdc++.h
+        popd
+    fi
+done
 popd
 
 ./main
+
 #g++ -o main -std=c++17 src/main.cpp -lpthread -lseccomp
 #valgrind -q --tool=memcheck ./main
 #rm main
